aa6243243676e5c2efd81004092d6bd080cf15d6
zlata <chernigovazlata2017@gmail.com>
2023-07-06 17:57:00 +0300

    Added burger-munu & update styles for header

---
+++ src/components/List/List.jsx
@@ -0,0 +1,13 @@
+import './list.scss';
+
+const List = () => (
+  <nav className='nav'>
+    <ul className='nav__list'>
+      <li className='nav__item'><a className='nav__link' href="ht">Home</a></li>
+      <li className='nav__item'><a className='nav__link' href="ht">Account</a></li>
+      <li className='nav__item'><a className='nav__link' href="ht">Games</a></li>
+    </ul>
+  </nav>
+)
+
+export default List;
